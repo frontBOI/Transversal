a4341deafbfc73ace50dc6c1e359166e7eff1226
tomBlanchet <tom.blanchet@lys-consulting.com>
2019-12-02 19:38:53 +0100

    CORS + refactor

--- Web/app.py
+++ Web/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_from_directory, jsonify
-from psycopg2.extras import RealDictCursor # transforms all retrieved rows as a dictionnary
 from multiprocessing import Array, Process, Value
+from collections import namedtuple
 from ctypes import c_char, c_wchar
+from flask_cors import CORS
 import psycopg2
+import requests
 import time
 import os
 
@@ -17,14 +19,13 @@ def queryDatabase():
     retVal = 'no data'
     try:
         connection = psycopg2.connect(user=POSTGRES_USER,
-                                        password=POSTGRES_PASSWORD,
-                                        host=POSTGRES_URL,
-                                        port=POSTGRES_PORT,
-                                        database=POSTGRES_DB_NAME)
+                                      password=POSTGRES_PASSWORD,
+                                      host=POSTGRES_URL,
+                                      port=POSTGRES_PORT,
+                                      database=POSTGRES_DB_NAME)
         cursor = connection.cursor()
-        postgreSQL_select_Query = "SELECT * FROM t_post"
-        cursor.execute(postgreSQL_select_Query)
-        queryResults = cursor.fetchall()
+        cursor.execute("SELECT * FROM t_post")
+        retVal = cursor.fetchall()
 
     except (Exception, psycopg2.Error) as error :
         print ("Error while fetching data from PostgreSQL", error)
@@ -35,12 +36,13 @@ def queryDatabase():
             cursor.close()
             connection.close()
 
-    print('returning')
-    print(queryResults)
-    return queryResults
+    return retVal
 
 # ROUTING
 app = Flask(__name__, static_folder='static/')
+# cors = CORS(app, resources={r"*": {"origins": "*"}})
+CORS(app)
+
 @app.route('/')
 def root():
     return send_from_directory(os.path.join('.', 'static'), 'index.html')
