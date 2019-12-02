d2ef417ad3dacdb82961047a23c3c4fcb6544d3d
tomBlanchet <tom.blanchet@lys-consulting.com>
2019-12-02 19:39:21 +0100

    Route calculation + moving marker

--- Web/static/script/script.js
+++ Web/static/script/script.js
@@ -23,6 +23,8 @@ function keepFooterAtBottom () {
 // --------------------------------------------------------------------------------------------------------------
 // @brief
 //  Initializes and sets up the Leaflet map
+// @source
+//   https://github.com/ewoken/Leaflet.MovingMarker
 function setupLeaflet () {
     var mymap = L.map('leafletMap').setView(
         [45.74846, 4.84671], // Lyon's geographical coordinates
@@ -46,4 +48,79 @@ function setupLeaflet () {
         fillOpacity: 0.5,
         radius: 500
     }).addTo(mymap);
+
+    const start = [4.84671,45.54846];
+    const end = [4.84671,45.94846];
+    fetchAndDisplayRoute(start, end, mymap);
+}
+
+
+// --------------------------------------------------------------------------------------------------------------
+// @brief
+//  Fetches the route computed by the OSRM online and free engine and displays it on the leaflet map 'mymap'
+// @note
+//  The 'from' and 'to' variables are arrays containing latitude and longitude data
+function fetchAndDisplayRoute (from, to, mymap) 
+{
+    fetch(`http://router.project-osrm.org/route/v1/driving/${from[0]},${from[1]};${to[0]},${to[1]}?overview=full`)
+    .then(r => r.json()).then(data => {
+        try {
+            data.routes.map(m => {
+                let decodedPolyline = L.polyline(
+                    polyline.decode(m.geometry), {
+                    color: 'red',
+                    weight: 3,
+                    opacity: 1
+                });
+                decodedPolyline.addTo(mymap);
+                addMovingMarker(decodedPolyline['_latlngs'], 6000, mymap);
+            });
+        } catch(e) {
+            console.error('Too many calls to the OSRM API. Try again later ('+e+')')
+        }
+    });
+}
+
+
+// --------------------------------------------------------------------------------------------------------------
+// @brief
+//  Adds a marker in the Leaflet map 'mymap' form the coordinates 'start' to 'end' and a duration of 'duration'
+// expressed in milliseconds
+function addMovingMarker (steps, duration, mymap) 
+{
+    // drawing the line the moving marker will follow
+    let coordinateArray = [...steps];
+    let myPolyline = L.polyline(coordinateArray);
+    myPolyline.addTo(mymap);
+
+    // adding start & end markers
+    L.marker(coordinateArray[0]).addTo(mymap);
+    L.marker(coordinateArray[coordinateArray.length - 1]).addTo(mymap);
+
+    // here is the moving marker (6 seconds animation)
+    let myMovingMarker = L.Marker.movingMarker(coordinateArray, duration, { autostart: false });
+    mymap.addLayer(myMovingMarker);
+    myMovingMarker.start();
+}
+
+
+// --------------------------------------------------------------------------------------------------------------
+// @brief
+//  Fetches all the incendie data from the PostgreSQL database and displays them inside the Leaflet map
+async function fetchAndDisplayIncendie () {
+    fetch('http://127.0.0.1:5000/data').then(r => r.json()).then(data => 
+    {
+        updateIncendieData(data)
+    })
+    .catch(e => { console.error(e) })
+}
+
+
+// --------------------------------------------------------------------------------------------------------------
+// @brief
+//  In the Leaflet map, updates all the displayed data with the new 'newDataset'
+function updateIncendieData (newDataset) {
+    for (let data of newDataset) {
+        console.log(data)
+    }
 }
