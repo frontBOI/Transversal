1ebb0d729c3a2729deecfd7bb959469552b16d89
Tom Blanchet <contact.tomblanchet@gmail.com>
2019-12-05 12:10:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # permet la détection de la technologie Python
+
+setup(
+    name='Projet transversal',
+    version='1.0',
+    long_description=__doc__,
+    packages=['Transversal'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask']
+)
